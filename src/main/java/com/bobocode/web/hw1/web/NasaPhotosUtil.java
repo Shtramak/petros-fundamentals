6379da27600c7fbfc6f809af73bca75ae45aa7eb
Andrii.Kovalchuk <shtramak@gmail.com>
2022-06-13 12:41:39 +0300

    add web package with hw1

---
+++ src/main/java/com/bobocode/web/hw1/web/NasaPhotosUtil.java
@@ -0,0 +1,27 @@
+package com.bobocode.web.hw1.web;
+
+import java.util.List;
+import java.util.Map;
+
+public class NasaPhotosUtil {
+    public static final String NASA_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=15&api_key=DEMO_KEY";
+
+    public static List<String> getPhotoUrlsUsingHttpClient(){
+        // use JsonNode
+        throw new UnsupportedOperationException();
+    }
+
+    public static List<String> getPhotoUrlsUsingWithRestTemplate(){
+        //use Jackson as messageConverter under the hood of the RestTemplate
+        throw new UnsupportedOperationException();
+    }
+
+    public static Map<String,Long> photosSizeByUrlUsingHttpClient(){
+        throw new UnsupportedOperationException();
+    }
+
+    public static Map<String,Long> photosSizeByUrlUsingRestTemplate(){
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/test/java/com/bobocode/web/hw1/warmup/WarmUpTest.java
@@ -0,0 +1,25 @@
+package com.bobocode.web.hw1.warmup;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static com.bobocode.web.hw1.warmup.WarmUp.sumOfPositiveSequenceFrom;
+import static com.bobocode.web.hw1.warmup.WarmUp.sumOfPrimes;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WarmUpTest {
+    @ParameterizedTest
+    @CsvSource(value = {"0,1,1", "0,3,6","1,2,5", "501, 20, 73052"})
+            void testSumOfPrimes(Integer skip, Integer numOfElements, Integer result) {
+        assertThat(sumOfPrimes(skip, numOfElements)).isEqualTo(result);
+    }
+
+    @Test
+    void testSumOfPositiveSequence(){
+        assertThat(sumOfPositiveSequenceFrom(List.of(1, 0, -1, 1, 2, 3))).isEqualTo(1);
+        assertThat(sumOfPositiveSequenceFrom(List.of(-4, -29, 5, 2, 3, -9, 12))).isEqualTo(10);
+    }
+}
