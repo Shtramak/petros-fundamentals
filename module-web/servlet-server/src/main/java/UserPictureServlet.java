c73ebd987439ecd2823632853668b369210208ac
Andrii.Kovalchuk <shtramak@gmail.com>
2022-07-04 15:34:29 +0300

    add UserPictureServlet exercise

---
+++ module-web/servlet-server/src/main/java/UserPictureServlet.java
@@ -0,0 +1,7 @@
+/*
+    Do everything require so this class will be able to handle POST request on /user-picture endpoint from hw3.warmup exercise
+    Response should be similar to "WELL DONE, {user.firstName}" with status 200 in case if the body matches with smallest picture attributes or
+    "{user.firstName} you can do better!" with status 400
+*/
+public class UserPictureServlet {
+}
